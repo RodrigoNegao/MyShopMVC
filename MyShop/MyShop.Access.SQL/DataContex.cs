51441bcdaab370205a7d0e4edd0011deaea01a60
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2020-02-07 19:04:30 -0300

    Add Entity Framework

---
+++ MyShop/MyShop.Access.SQL/DataContex.cs
@@ -0,0 +1,22 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Access.SQL
+{
+    class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Proucts { get; set; }
+        public DbSet<ProductCategory> ProuctsCategories { get; set; }
+    }
+}
